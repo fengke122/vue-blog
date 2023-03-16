752bff20e452a2f76651c2992bc0fa7d91a6ff95
fengke122 <1443417579@qq.com>
2023-03-16 18:56:15 +0800

    修改了security页面的交互逻辑

--- src/views/Changepsw.vue
+++ src/views/Changepsw.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="change-body">
-    <div class="container">
+    <div class="container" v-loading="loading">
       <div class="setps-box">
         <el-steps :space="150" :active="active" finish-status="success">
           <el-step title="验证身份"></el-step>
@@ -10,23 +10,25 @@
       </div>
       <div class="setp1" v-show="showingContent === 0">
         <div class="changepsw-box">
-          <el-input v-model="phone" placeholder="请输入手机号" class="phone-input"></el-input>
+          <el-input v-model="email" placeholder="请输入邮箱" class="email-input"></el-input>
           <el-row :gutter="20" class="input-box">
             <el-col :span="12" :push="1"><el-input v-model="code" placeholder="请输入验证码" ></el-input></el-col>
-            <el-col :span="5" :push="1">    <el-button type="primary" class="send-code" :disabled="disableButton"  @click="sendCode">
-              {{ remainingTime > 0 ? `${remainingTime}s后获取` : '发送验证码' }}
-            </el-button></el-col>
+            <el-col :span="5" :push="1">
+              <el-button type="primary" class="send-code" :disabled="disableButton"  @click="sendCode">
+                {{ remainingTime > 0 ? `${remainingTime}s后获取` : '发送验证码' }}
+              </el-button></el-col>
           </el-row>
         </div>
         <div style="text-align: center;"><el-button style="margin-top: 12px;" type="primary" @click="next"  :disabled="!canNext" class="next-btn">下一步</el-button></div>
       </div>
-      <div class="setp2" v-show="showingContent === 1">
+      <div class="setp2" v-show="showingContent === 1" >
         <div style="text-align: center; " >
-          <el-input v-model="newpassword" style="width: 350px" placeholder="请输入新密码" ></el-input>
+          <el-input v-model="newPassword" style="width: 350px" placeholder="请输入新密码" ></el-input>
+          <p class="tip" v-if="isInvalid">请输入8-16位数字和英文字母组合密码</p>
         </div>
         <div style="text-align: center;">
         </div>
-        <div style="text-align: center;"><el-button style="margin-top: 12px;" type="primary" @click="() => {next(); sendNewpsw();}"   class="next-btn">下一步</el-button></div>
+        <div style="text-align: center;"><el-button style="margin-top: 12px;" type="primary" @click="() => {sendNewpsw(); }"  class="next-btn">下一步</el-button></div>
       </div>
       <div class="setp3" v-show="showingContent === 2">
         <div style="text-align: center;">
@@ -44,18 +46,35 @@ export default {
   data() {
     return {
       active: 1,
-      phone: '',
+      email: '',
       code: '',
-      phonecode: '1234',
+      emailcode: '1234',
+      isSending: false,
       remainingTime: 0,
       disableButton: false,
       showingContent: 0,
-      newpassword:'',
-      canNext: false // 下一步按钮是否可点击
+      newPassword:'',
+      canNext: false, // 下一步按钮是否可点击
+      canNext2: false,
+      loading: false
     };
   },
   methods: {
     next() {
+      if (this.active++ > 2) this.active = 1;
+      if (this.showingContent === 2) {
+        // 如果是最后一个盒子了，则回到第一个盒子
+        this.showingContent = 0
+      } else {
+        // 否则显示下一个盒子
+        this.showingContent += 1
+      }
+    },
+    next2() {
+      if (this.isInvalid) return;
+      if (!this.canNext2) {
+        return;
+      }
       if (this.active++ > 2) this.active = 1;
       if (this.showingContent === 2) {
         // 如果是最后一个盒子了，则回到第一个盒子
@@ -75,54 +94,71 @@ export default {
 
       // 发送验证码的逻辑，此处只是模拟发送请求
       // 此处应该调用真实的发送验证码接口
-      this.$http.post('/your/api/endpoint', { phoneNumber: this.phone }).then(response => {
-        // 请求成功后执行倒计时逻辑
-        this.isSending = false;
-
-        // 获取请求返回的验证码
-        this.phonecode = response.data.phonecode;
-
-        this.remainingTime = 60;
-        const countdownTimer = setTimeout(() => {
-          this.disableButton = false;
-        }, 60000);
-
-        const updateTimer = setInterval(() => {
-          this.remainingTime--;
-          if (this.remainingTime === 0) {
-            clearInterval(updateTimer);
-            clearTimeout(countdownTimer);
-            this.disableButton = false;
-          }
-        }, 1000);
-      }).catch(error => {
-        // 请求失败处理逻辑
-        console.error(error);
-        this.isSending = false;
+      this.remainingTime = 60;
+      const countdownTimer = setTimeout(() => {
         this.disableButton = false;
-        // TODO: show error message to user
-      });
-    },
-    sendNewpsw() {
-      const newPassword = this.newPassword; // 获取输入框的值
-      this.$http.post('/do', { newpassword: newPassword })
+      }, 60000);
+
+      const updateTimer = setInterval(() => {
+        this.remainingTime--;
+        if (this.remainingTime === 0) {
+          clearInterval(updateTimer);
+          clearTimeout(countdownTimer);
+          this.disableButton = false;
+        }
+      }, 1000);
+      const url = `/user/sendEmail?email=${this.email}`;
+      this.$http.get(url)
           .then(response => {
-            console.log(response.data); // 请求成功后的处理
+            // 请求成功后执行倒计时逻辑
+            this.isSending = false;
+            // 获取请求返回的验证码
+            this.emailcode = response.data.emailcode;
           })
           .catch(error => {
-            console.log(error); // 请求失败后的处理
-          });
+            // 请求失败处理逻辑
+            console.error(error);
+            this.isSending = false;
+          })
+    },
+    sendNewpsw() {
+      if (this.isInvalid) {
+        return;
+      }
+      else {
+        this.loading = true
+        const url = `/user/changePassword?email=${this.newPassword}`;
+        this.$http.get(url)
+            .then(response => {
+              console.log(response.data)
+              this.loading = false
+              this.showingContent += 1
+            })
+            .catch(error => {
+              console.log(error)
+              this.loading = false
+              alert('修改密码失败')
+            })
+
+      }
+
     }
   },
   watch: {
     code(val) {
       if (this.showingContent === 0) {
-        if (val === this.phonecode) {
+        if (val === this.emailcode) {
           // 如果输入正确，可以进行下一步操作
           this.canNext = true
         }
       }
-      },
+    },
+  },
+  computed:{
+    isInvalid() {
+      const pattern = /^(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]{8,16}$/
+      return !pattern.test(this.newPassword)
+    }
   }
 }
 </script>
@@ -163,7 +199,7 @@ export default {
   margin-bottom: 50px;
 }
 
-.phone-input {
+.email-input {
   width: 348px;
   position: relative;
   left: 50px;
@@ -220,5 +256,9 @@ export default {
   text-decoration: none;
 }
 
+.tip {
+  color: rgba(255,255,255,0.6);
+  font-size: 16px;
+}
 
 </style>
